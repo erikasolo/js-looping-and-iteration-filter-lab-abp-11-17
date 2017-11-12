6bafdafeaef2bbebbeafc9f3ab46d813f82085ed
Erika Solomon <mrserikasolomon@gmail.com>
2017-11-12 18:08:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file
+function findMatching (drivers, name){
+  let matchingDrivers = drivers.filter(function(driver){
+      if (name.toLowerCase()=== driver. toLowerCase()){
+        else{
+        return
+        }
+      }
+  })
+
+
+  
+}
